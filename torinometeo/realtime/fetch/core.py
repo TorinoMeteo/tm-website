c12a273e169c5a6141c927349f2365889897756d
abidibo <abidibo@gmail.com>
2016-12-13 10:29:07 +0100

    fake agent

--- torinometeo/realtime/fetch/core.py
+++ torinometeo/realtime/fetch/core.py
@@ -38,7 +38,8 @@ class Data(dict):
 def fetch(url):
     """ Fetches an url content
     """
-    response = requests.get(url).text
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} # noqa
+    response = requests.get(url, headers=headers).text
     return response
 
 
