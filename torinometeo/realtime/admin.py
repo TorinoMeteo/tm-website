a8ca8a49f069c5705231f2e59501504cbb354e9b
abidibo <abidibo@gmail.com>
2016-12-14 13:13:13 +0100

    undo

--- torinometeo/realtime/admin.py
+++ torinometeo/realtime/admin.py
@@ -26,9 +26,8 @@ admin.site.register(Province, ProvinceAdmin)
 # stations
 
 class StationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'nation', 'region', 'city', 'data_format', 'data_url', 'webcam', 'test_fetch', 'data_date_format', 'data_time_format', ]
+    list_display = ['id', 'name', 'nation', 'city', 'data_format', 'data_url', 'webcam', 'test_fetch', ]
     list_filter = ('region', 'data_format', )
-    list_editable = ('data_date_format', 'data_time_format', )
     search_fields = ('name', )
     prepopulated_fields = {'slug': ('name',)}
 
