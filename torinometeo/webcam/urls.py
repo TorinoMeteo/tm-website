de01e7a70aec344220057eda5249aafb84b0f9e5
abidibo <abidibo@gmail.com>
2016-12-23 15:34:33 +0100

    async webcam

--- torinometeo/webcam/urls.py
+++ torinometeo/webcam/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns,url
-from webcam.views import WebcamDetailView
+from django.conf.urls import patterns, url
+from webcam.views import WebcamDetailView, WebcamAsyncView
 
 urlpatterns = patterns('webcam.views',
-    url(r'^(?P<slug>[-\w]+)/$', WebcamDetailView.as_view(), name='webcam-detail'),
+    url(r'^async/(?P<slug>[-\w]+)/$', WebcamAsyncView.as_view(), name='webcam-async'), # noqa
+    url(r'^(?P<slug>[-\w]+)/$', WebcamDetailView.as_view(), name='webcam-detail'), # noqa
 )
-
--- torinometeo/webcam/views.py
+++ torinometeo/webcam/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, View
+# from django.shortcuts import get_object_or_404
 
-from webcam.models import Webcam
+from .models import Webcam
+
 
 class WebcamDetailView(DetailView):
     model = Webcam
     template_name = 'webcam/detail.html'
+
+
+class WebcamAsyncView(View):
+    def get(self, request, slug):
+        w = Webcam.objects.get(slug=slug)
+        return render(
+            request,
+            'webcam/async.html',
+            {'webcam': w}
+        )
