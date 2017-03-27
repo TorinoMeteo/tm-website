9f017fbded8f7d6419f94cdaf71ef1ebc429d033
abidibo <abidibo@gmail.com>
2017-03-27 12:45:22 +0200

    analytics and cookie-script

--- torinometeo/core/settings/common.py
+++ torinometeo/core/settings/common.py
@@ -136,6 +136,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.debug',
                 'treenav.context_processors.treenav_active',
             ],
         },
